38d030fa862a77922cdac1e8ea5a1c0693623b58
jboeh03 <130064065+jboeh03@users.noreply.github.com>
2023-04-06 12:33:25 -0400

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -6,7 +6,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib
 
-coin_app = Flask(__name__,
+app = Flask(__name__,
             template_folder=os.path.abspath('../frontend/templates'),
             static_folder=os.path.abspath('../frontend/static'))
 alerts = {}
