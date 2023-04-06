fb5f3aba49a3bd4558dc7e72db35d5e2843d6bf7
jboeh03 <130064065+jboeh03@users.noreply.github.com>
2023-04-06 12:53:13 -0400

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -6,9 +6,9 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib
 
-app = Flask(__name__),
-template_folder=os.path.abspath('../frontend/templates'),
-static_folder=os.path.abspath('../frontend/static'))
+app = Flask(__name__,
+            template_folder=os.path.abspath('../frontend/templates'),
+            static_folder=os.path.abspath('../frontend/static'))
 alerts = {}
 
 EMAIL_ADDRESS = "yourpricealerts@gmail.com"
